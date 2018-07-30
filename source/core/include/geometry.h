dd64be08b918c70b622c696092c7b83863da43a7
Tom Moll <absentisinaventulus@gmail.com>
2018-07-30 10:52:58 -0700

    Updated Vector2 with << operator

--- source/core/include/geometry.h
+++ source/core/include/geometry.h
@@ -4,19 +4,36 @@
 
 namespace geometry {
     template <typename T> class Vector2 {
-    public:
-        Vector2();
-        Vector2(T x, T y);
-        bool HasNaNs();
-        friend std::ostream& operator<<(std::ostream& s, const Vector2& v);
-        T x, y;
-    };
-    template <typename T> class Vector3 {
         public:
+            Vector2() {
+                x = y = 0;
+            }
 
-            // Public Vector3 Data
-            T x, y, z;
+            Vector2(T x, T y) {
+                this->x = x;
+                this->y = y;
+                assert(!HasNaNs());
+            }
+
+            bool HasNaNs() {
+                return isnan(x) || isnan(y);
+            }
+
+            friend std::ostream& operator<<(std::ostream& s, const Vector2& v) {
+                s << "Vector2<" << v.x << "," << v.y << ">";
+                return s;
+            }
+
+            // Public Vector2 Data
+            T x, y;
     };
+
+    template <typename T> class Vector3 {
+    public:
+        // Public Vector3 Data
+        T x, y, z;
+    };
+
     typedef Vector2<float> Vector2f;
     typedef Vector2<int> Vector2i;
     typedef Vector3<float> Vector3f;
