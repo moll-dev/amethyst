b8746d80a71455fc74ff61ed881effb1098a78f6
Tom Moll <tmoll@linkedin.com>
2018-08-06 22:43:55 -0700

    Implemented moooost of the Vec3 class / Vec2 class

--- source/amethyst/source/amethyst.cpp
+++ source/amethyst/source/amethyst.cpp
@@ -2,23 +2,28 @@
 #include <geometry.h>
 
 using namespace std;
-using geometry::Vector2;
+using namespace geometry;
 
 int main(){
-    cout << "Vector Addition" << endl;
+    cout << "Vector Addition" << endl << endl;
 
-    Vector2<float> v1 = Vector2<float>(6.55f, 9.0f);
-    Vector2<float> v2 = Vector2<float>(4.0f, 1.0f);
-    Vector2<float> v3 = Vector2<float>(2.0f, 2.0f);
+    Vector2f v1 = Vector2f(6.55f, 9.0f);
+    Vector2f v2 = Vector2f(4.0f, 1.0f);
+    Vector2f v3 = Vector2f(2.0f, 2.0f);
 
-    v1 += Vector2<float>(4.0f, 4.0f);
+    v1 += Vector2f(4.0f, 4.0f);
 
     cout << "V1:" << v1 << endl;
     cout << "V2:" << v2 << endl;
 
-    //cout << v1 + v2 << endl;
     cout << Vector2<int>(20, 40) / 10 << endl;
+    cout << Vector2<float>(5.0f, 6.0f) * 10.0f << endl;
     cout << 10.0f * Vector2<float>(5.0f, 6.0f) << endl;
 
+    cout << "DOT PRODUCTS " << Dot(Vector2f(5.0, 5.0), Vector2f(1.0f, 1.0f)) << endl;
+    cout << "DOT PRODUCTS " << Dot(Vector2f(1.0, 0.0).Normal(), Vector2f(0.0f, 1.0f).Normal()) << endl;
+
+    cout << "Normal:  " << Normal(Vector3f(1.0f, 0.5f, 0.0f)) << endl;
+    cout << "Normal2: " << Vector3f(1.0f, 0.5f, 0.0f).Normal() << endl;
     return 0;
 }
--- source/core/include/geometry.h
+++ source/core/include/geometry.h
@@ -19,7 +19,15 @@ namespace geometry {
                 return isnan(x) || isnan(y);
             }
 
-            Vector2<T> operator+(const Vector2& v) {
+            float Length() const {
+                return std::sqrt((x * x + y * y));
+            }
+
+            Vector2<T> Normal() const {
+                return Vector2<T>(x / Length(), y / Length());
+            }
+
+            Vector2<T> operator+(const Vector2& v) const {
                 return Vector2(x+v.x, y+v.y);
             }
 
@@ -28,7 +36,7 @@ namespace geometry {
                 return *this;
             }
 
-            Vector2<T> operator-(const Vector2& v) {
+            Vector2<T> operator-(const Vector2& v) const {
                 return Vector2(x-v.x, y-v.y);
             }
 
@@ -41,7 +49,7 @@ namespace geometry {
                 return *this;
             }
 
-            Vector2<T> operator*(T s) {
+            Vector2<T> operator*(T s) const {
                 return Vector2<T>(s*x, s*y);
             }
 
@@ -50,7 +58,7 @@ namespace geometry {
                 return *this;
             }
 
-            Vector2<T> operator/(T f) {
+            Vector2<T> operator/(T f) const {
                 assert(f != 0);
                 float inv = (float)1 / f;
                 return Vector2(x * inv, y * inv);
@@ -72,22 +80,127 @@ namespace geometry {
             T x, y;
     };
 
-    template <typename T> class Vector3 {
-    public:
-        // Public Vector3 Data
-        T x, y, z;
-    };
-
-
     typedef Vector2<float> Vector2f;
     typedef Vector2<int> Vector2i;
+
+
+    template <typename T> class Vector3 {
+    public:
+            Vector3() {
+                x = y = z = 0;
+            }
+
+            Vector3(T x, T y, T z) {
+                this->x = x;
+                this->y = y;
+                this->z = z;
+                assert(!HasNaNs());
+            }
+
+            bool HasNaNs() {
+                return isnan(x) || isnan(y) || isnan(z);
+            }
+
+            float Length() const {
+                return std::sqrt(x * x + y * y + z * z);
+            }
+
+            Vector3<T> Normal() const {
+                return Vector3<T>(x / Length(), y / Length(), z / Length());
+            }
+
+            Vector3<T> operator+(const Vector3& v) const {
+                return Vector3(x + v.x, y + v.y, z + v.z);
+            }
+
+            Vector3<T>& operator+=(const Vector3& v) {
+                x += v.x; y += v.y; z += v.z;
+                return *this;
+            }
+
+            Vector3<T> operator-(const Vector3& v) const {
+                return Vector3(x - v.x, y - v.y, z - v.z);
+            }
+
+            Vector3<T> operator-() const {
+                return Vector3<T>(-x, -y, -z);
+            }
+
+            Vector3<T>& operator-=(const Vector3& v) {
+                x -= v.x; y -= v.y; z -= v.z;
+                return *this;
+            }
+
+            Vector3<T> operator*(T s) const {
+                return Vector3<T>(s * x, s * y, s * z);
+            }
+
+            Vector3<T>& operator*=(T s) {
+                x *= s; y *= s; z *= s;
+                return *this;
+            }
+
+            Vector3<T> operator/(T f) const {
+                assert(f != 0);
+                float inv = (float)1 / f;
+                return Vector3(x * inv, y * inv, z * inv);
+            }
+
+            Vector3<T>& operator/=(T f) {
+                assert(f != 0);
+                float inv = (float)1 / f;
+                x *= inv; y *= inv; z *= inv;
+                return *this;
+            }
+
+            friend std::ostream& operator<<(std::ostream& s, const Vector3& v) {
+                s << "Vector3<" << v.x << "," << v.y << "," << v.z << ">";
+                return s;
+            }
+
+            // Public Vector2 Data
+            T x, y, z;
+    };
+
     typedef Vector3<float> Vector3f;
     typedef Vector3<int> Vector3i;
 
-    // Inlined functions
-    template <typename T>
-    inline Vector2<T> operator*(T s, const Vector2<T> &v) {
+    // Vector 2 In-Line
+    template <typename T> inline Vector2<T>
+    operator*(T s, const Vector2<T> &v) {
         return v * s;
     }
+
+    template <typename T> inline T
+    Dot(const Vector2<T> &v1, const Vector2<T> &v2) {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+
+    template <typename T> inline Vector2<T>
+    Normal(const Vector2<T> &v) {
+        return Vector2<T>(v.x / v.Length(), v.y / v.Length());
+    }
+
+    // Vector 3 In-Line
+    template <typename T> inline Vector3<T>
+    operator*(T s, const Vector3<T> &v) {
+        return v * s;
+    }
+
+    template <typename T> inline T
+    Dot(const Vector3<T> &v1, const Vector3<T> &v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v1.z;
+    }   
+
+    template <typename T> inline Vector3<T>
+    Normal(const Vector3<T> &v) {
+        return Vector3<T>(v.x / v.Length(), v.y  / v.Length(), v.z / v.Length());
+    }
+    
+    // // Inlined functions
+    // template <typename T>
+    // inline Vector2<T> operator*(T s, const Vector2<T> &v) {
+    //     return v * s;
+    // }
 
 }
